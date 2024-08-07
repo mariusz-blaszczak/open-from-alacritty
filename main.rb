4dcc4b0f738623c7346c78bb101953994e5a25b3
Mariusz Błaszczak <mariusz.blaszczak@akra.net>
2024-08-07 09:33:10 +0200

    update hardcoded strings

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@
 require "logger"
 
 # Initialize the logger
-log_file_path = '/Users/mariusz/Projects/Ruby/open-from-alacritty/logfile.log'
+log_file_path = '/Users/mblaszczak/Projects/Ruby/open-from-alacritty/logfile.log'
 logger = Logger.new(log_file_path)
 logger.info('Script started.')
 # arguments
@@ -12,8 +12,9 @@ file_path, line_number = file_path_with_line_number.split(":")
 
 command_parts = []
 
-command_parts << "/Applications/RubyMine.app/Contents/MacOS/rubymine"
+command_parts << "'/Users/mblaszczak/Library/Application Support/JetBrains/Toolbox/scripts/rubymine'"
 command_parts << "--line #{line_number}" unless line_number.nil?
 command_parts << file_path
+logger.info(command_parts.join(' '))
 
 system(command_parts.join(" "))
